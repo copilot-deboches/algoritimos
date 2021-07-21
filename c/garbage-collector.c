18d2fbf81e1e8ec456f8f8dce2e94e24685c1813
RodrigoDornelles <rodrigo@dornelles.me>
2021-07-20 23:48:05 -0300

    Create garbage-collector.c

---
+++ c/garbage-collector.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+
+/**
+ * garbage collector
+ */
+int main() 
+{
+  int *p = NULL;
+  int *q = NULL;
+  int *r = NULL;
+  int *s = NULL;
+  int *t = NULL;
+  int *u = NULL;
+  int *v = NULL;
+  int *w = NULL;
+  int *x = NULL;
+  int *y = NULL;
+  int *z = NULL;
+  int *a = NULL;
+  int *b = NULL;
+  int *c = NULL;
+  int *d = NULL;
+  int *e = NULL;
+  int *f = NULL;
+  int *g = NULL;
+  int *h = NULL;
+  int *i = NULL;
+  int *j = NULL;
+  int *k = NULL;
+  int *l = NULL;
+  int *m = NULL;
+  int *n = NULL;
+  int *o = NULL;
+  int *p1 = NULL;
+  int *p2 = NULL;
+  int *p3 = NULL;
+  int *p4 = NULL;
+  int *p5 = NULL;
+  int *p6 = NULL;
+  int *p7 = NULL;
+  int *p8 = NULL;
+  int *p9 = NULL;
+  int *p10 = NULL;
+  int *p11 = NULL;
+  int *p12 = NULL;
+  int *p13 = NULL;
+  int *p14 = NULL;
+  int *p15 = NULL;
+  int *p16 = NULL;
+  int *p17 = NULL;
+  int *p18 = NULL;
+  int *p19 = NULL;
+  int *p20 = NULL;
+  int *p21 = NULL;
+  int *p22 = NULL;
+  int *p23 = NULL;
+  int *p24 = NULL;
+  int *p25 = NULL;
+  int *p26 = NULL;
+  int *p27 = NULL;
+  int *p28 = NULL;
+  int *p29 = NULL;
+  int *p30 = NULL;
+  int *p31 = NULL;
+  int *p32 = NULL;
+}
